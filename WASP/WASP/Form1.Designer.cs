539e063f442b972a59ea4e09fce4299039780c83
Muhammad Razali <wireless-@hotmail.com>
2022-01-16 23:44:38 +0800

    init

---
+++ WASP/WASP/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WASP
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.navigationPanel = new System.Windows.Forms.Panel();
+            this.settingsPanel = new System.Windows.Forms.Panel();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.settingsRightPanel = new System.Windows.Forms.Panel();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.dashboardPanel = new System.Windows.Forms.Panel();
+            this.dashboardButton = new System.Windows.Forms.Button();
+            this.dashboardRightPanel = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.navigationPanel.SuspendLayout();
+            this.settingsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.dashboardPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // navigationPanel
+            // 
+            this.navigationPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.navigationPanel.Controls.Add(this.settingsPanel);
+            this.navigationPanel.Controls.Add(this.logoPictureBox);
+            this.navigationPanel.Controls.Add(this.dashboardPanel);
+            this.navigationPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.navigationPanel.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.navigationPanel.Location = new System.Drawing.Point(0, 0);
+            this.navigationPanel.Name = "navigationPanel";
+            this.navigationPanel.Size = new System.Drawing.Size(200, 577);
+            this.navigationPanel.TabIndex = 0;
+            // 
+            // settingsPanel
+            // 
+            this.settingsPanel.Controls.Add(this.settingsButton);
+            this.settingsPanel.Controls.Add(this.settingsRightPanel);
+            this.settingsPanel.Location = new System.Drawing.Point(3, 215);
+            this.settingsPanel.Name = "settingsPanel";
+            this.settingsPanel.Size = new System.Drawing.Size(200, 88);
+            this.settingsPanel.TabIndex = 4;
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.settingsButton.FlatAppearance.BorderSize = 0;
+            this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsButton.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.settingsButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(168)))), ((int)(((byte)(14)))));
+            this.settingsButton.Image = ((System.Drawing.Image)(resources.GetObject("settingsButton.Image")));
+            this.settingsButton.Location = new System.Drawing.Point(0, 0);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(197, 88);
+            this.settingsButton.TabIndex = 1;
+            this.settingsButton.Text = "Settings";
+            this.settingsButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.settingsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.settingsButton.UseVisualStyleBackColor = true;
+            // 
+            // settingsRightPanel
+            // 
+            this.settingsRightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.settingsRightPanel.Location = new System.Drawing.Point(190, 0);
+            this.settingsRightPanel.Name = "settingsRightPanel";
+            this.settingsRightPanel.Size = new System.Drawing.Size(10, 88);
+            this.settingsRightPanel.TabIndex = 3;
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.BackgroundImage")));
+            this.logoPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.logoPictureBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logoPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(200, 125);
+            this.logoPictureBox.TabIndex = 1;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // dashboardPanel
+            // 
+            this.dashboardPanel.Controls.Add(this.dashboardButton);
+            this.dashboardPanel.Controls.Add(this.dashboardRightPanel);
+            this.dashboardPanel.Location = new System.Drawing.Point(0, 121);
+            this.dashboardPanel.Name = "dashboardPanel";
+            this.dashboardPanel.Size = new System.Drawing.Size(200, 88);
+            this.dashboardPanel.TabIndex = 3;
+            // 
+            // dashboardButton
+            // 
+            this.dashboardButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dashboardButton.FlatAppearance.BorderSize = 0;
+            this.dashboardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dashboardButton.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dashboardButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(168)))), ((int)(((byte)(14)))));
+            this.dashboardButton.Image = ((System.Drawing.Image)(resources.GetObject("dashboardButton.Image")));
+            this.dashboardButton.Location = new System.Drawing.Point(0, 0);
+            this.dashboardButton.Name = "dashboardButton";
+            this.dashboardButton.Size = new System.Drawing.Size(194, 88);
+            this.dashboardButton.TabIndex = 1;
+            this.dashboardButton.Text = "Dashboard";
+            this.dashboardButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dashboardButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.dashboardButton.UseVisualStyleBackColor = true;
+            this.dashboardButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dashboardRightPanel
+            // 
+            this.dashboardRightPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(168)))), ((int)(((byte)(14)))));
+            this.dashboardRightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dashboardRightPanel.Location = new System.Drawing.Point(195, 0);
+            this.dashboardRightPanel.Name = "dashboardRightPanel";
+            this.dashboardRightPanel.Size = new System.Drawing.Size(5, 88);
+            this.dashboardRightPanel.TabIndex = 3;
+            // 
+            // closeButton
+            // 
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.closeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(168)))), ((int)(((byte)(14)))));
+            this.closeButton.Location = new System.Drawing.Point(899, 12);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(25, 23);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "X";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.navigationPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.navigationPanel.ResumeLayout(false);
+            this.settingsPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.dashboardPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel navigationPanel;
+        private PictureBox logoPictureBox;
+        private Button dashboardButton;
+        private Button closeButton;
+        private Panel dashboardPanel;
+        private Panel dashboardRightPanel;
+        private Panel settingsPanel;
+        private Button settingsButton;
+        private Panel settingsRightPanel;
+    }
+}
---
+++ WASP/WASP/Form1.cs
@@ -0,0 +1,20 @@
+namespace WASP
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
