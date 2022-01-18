87a8548c2971b889068477309d4752232e09ee91
Muhammad Razali <wireless-@hotmail.com>
2022-01-18 22:27:47 +0800

    Added LolBin
    LolBin contains the type of lolbin we are focussingon. It can have 1 or more ways of vulnerabilities to be all fully tested.

---
+++ WASP/WASP/Common/Models/LolBin.cs
@@ -0,0 +1,28 @@
+﻿using WASP.Common.Interfaces;
+
+namespace WASP.Common.Models
+{
+    public class LolBin
+    {
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public List<IVulnerability> Vulnerabilities { get; private set; }
+
+
+        public LolBin(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            Vulnerabilities = new List<IVulnerability>();
+        }
+
+        public LolBin RegisterVulnerability(IVulnerability vulnerability)
+        {
+            Vulnerabilities.Add(vulnerability);
+
+            return this;
+        }
+    }
+}
